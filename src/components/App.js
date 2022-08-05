53b22a97e694f1ad552789d869ddbb73ec7cf251
Kevin <magavakevin10@gmail.com>
2022-08-05 10:20:02 -0400

    commit tree 20ca33daec5b0ff46196c6fb3ede426fc8e21ba0

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      {/* You're on your own from here! Follow the deliverables; test things out in
+      the browser as you write your code; and good luck! */}
+      <Header name={blogData.name} />
+      <About image={blogData.image} />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
 
 export default App;
+
+//To export multiple use: export { variable1, variable2, function1, etc}
+//To import multiple use: import App, {variable1, function1, etc} from "./blah"
