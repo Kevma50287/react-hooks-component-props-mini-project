0bbf7506f14d839a0a6919420152967c96e057da
Kevin <magavakevin10@gmail.com>
2022-08-08 12:46:43 -0400

    Done

--- src/components/Article.js
+++ src/components/Article.js
@@ -24,7 +24,7 @@ function Article({ title, date = "January 1, 1970", preview, minutes}) {
   return (
     <article>
         <h3>{title}</h3>
-        <small>{date}{emoji(minutes)}{minutes} min read</small>
+        <small>{date}{emoji(minutes)} || {minutes} min read</small>
         <p>{preview}</p>
     </article>
     
